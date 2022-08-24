7d7c2917f67b28e44aabbdd9a2c33684c31ca12c
myname <2214736768@qq.com>
2022-08-24 20:29:08 +0800

    用户登录后端不再返回User类 而是改为向前端返回ResultData类包含响应码等数据

--- src/com/zldx/service/impl/LoginImpl.java
+++ src/com/zldx/service/impl/LoginImpl.java
@@ -2,26 +2,49 @@ package com.zldx.service.impl;
 
 import com.zldx.dao.UserDao;
 import com.zldx.dao.impl.UserDaoImpl;
+import com.zldx.pojo.ResultData;
 import com.zldx.pojo.User;
 import com.zldx.service.LoginService;
 
 import java.sql.SQLException;
 
 public class LoginImpl implements LoginService {
-        private  UserDao userDao = new UserDaoImpl();
+        private final UserDao userDao = new UserDaoImpl();
+
     @Override
-    public User login(String username, String password) {
+    public ResultData checkUserName(String username) {//验证用户名是否存在
         try {
             User user = userDao.selectName(username);
-            if(user != null){
-                if(username.equals(user.getUsername()) && password.equals(user.getPassword())){
-                    return user;
-                }
+            if(user != null && user.getUsername().equals(username)){ //用户存在
+                return new ResultData(0,"用户存在");
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return null;
+        return new ResultData(100,"用户不存在");
+    }
+
+    @Override
+    public ResultData login(String username, String password) {
+        try {
+            User user = userDao.selectName(username);
+            if(user != null){
+                if(!username.equals(user.getUsername())){
+                    return new ResultData(100,"用户名错误(注意区分大小写)");
+                }
+                if (!password.equals(user.getPassword())){
+                    return new ResultData(100,"用户密码不正确");
+                }
+                //防止密码传入前端，清空密码
+                user.setPassword("");
+                return new ResultData(0,"登录成功",user);
+            }else {
+                return new ResultData(100,"用户名错误");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return new ResultData(100,"登录失败");
     }
 }
 
